bcd4919f4ca56ad005e33ef3c778104737a971b6
prof79 <markus@markusegger.at>
2025-02-28 19:52:24 +0100

    Double spaces fixed.

--- PoshWinRT/Properties/AssemblyInfo.cs
+++ PoshWinRT/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PoshWinRT")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
