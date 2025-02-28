e199fcb8c54aa26620affc65ff5997c55a4dc4ab
prof79 <markus@markusegger.at>
2025-02-28 20:09:42 +0100

    Added IAsyncOperationWithProgress wrapper.

---
+++ PoshWinRT/AsyncOperationWithProgressWrapper.cs
@@ -0,0 +1,41 @@
+﻿// AsyncOperationWithProgressWrapper.cs
+
+namespace PoshWinRT
+{
+    using System;
+    using System.Threading.Tasks;
+    using Windows.Foundation;
+
+    public class AsyncOperationWithProgressWrapper<TResult, TProgress>
+        : AsyncInfoWrapper<IAsyncOperationWithProgress<TResult, TProgress>>
+    {
+        public AsyncOperationWithProgressWrapper(object asyncOperation)
+            : base(asyncOperation)
+        { }
+
+        public object AwaitResult()
+        {
+            return AwaitResult(-1);
+        }
+
+        public object AwaitResult(int millisecondsTimeout)
+        {
+            var task = _asyncInfo.AsTask();
+
+            task.Wait(millisecondsTimeout);
+
+            if (task.IsCompleted)
+            {
+                return task.Result;
+            }
+            else if (task.IsFaulted)
+            {
+                throw task.Exception;
+            }
+            else
+            {
+                throw new TaskCanceledException(task);
+            }
+        }
+    }
+}
